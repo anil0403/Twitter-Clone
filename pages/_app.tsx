63bb18f2f92202d428df7fc4ec577d9d0ab6255f
anil <anilshrestha0403.as@gmail.com>
2023-04-10 12:36:34 +0545

    conditional button component created

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,8 +3,10 @@ import Layout from "@/components/Layout";
 import "@/styles/globals.css";
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
