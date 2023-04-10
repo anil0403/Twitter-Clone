c8cfd2977a006142de3a3726c8d777cb943a62e8
anil <anilshrestha0403.as@gmail.com>
2023-04-10 12:11:04 +0545

    home page initialized

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
-
+import Header from "@/components/Header"
 export default function Home() {
   return (
-    <div className="text-3xl text-sky-500"> Hello Workd</div>
+    <>
+    <Header  label ="Home" />
+    </>
   )
 }
